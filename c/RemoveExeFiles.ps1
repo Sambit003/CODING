6804487b01949accfa8c5d14f577e8291da5d1b8
Sambit Chakraborty <smbtchakraborty@gmail.com>
2023-11-14 14:21:43 +0530

    Move: move the removal script to master directory

--- c/RemoveExeFiles.ps1
+++
@@ -1,7 +0,0 @@
-$directoryPath = Get-Location
-$exeFiles = Get-ChildItem -Path $directoryPath -Filter *.exe -Recurse
-foreach ($file in $exeFiles) {
-    Write-Host "Removing $($file.FullName)"
-    Remove-Item -Path $file.FullName -Force
-}
-Write-Host "Execution completed. All .exe files removed."
